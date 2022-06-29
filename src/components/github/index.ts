e6d986e9ebca949e7e18dcbf3f99ccec13c26bb0
KaMeHb-UA <marlock@bassteam.ml>
2022-06-29 10:37:20 +0300

    use .map() instead of .push() in github component

--- src/components/github/index.ts
+++ src/components/github/index.ts
@@ -1,5 +1,8 @@
 import { Octokit } from "@octokit/core";
 import { paginateRest } from "@octokit/plugin-paginate-rest";
+import fromAsyncUntyped from 'array-from-async';
+
+const fromAsync = fromAsyncUntyped as <T>(iterable: T) => Promise<Array<T extends AsyncIterable<infer R> ? R : never>>
 
 const OctokitWithPagination = Octokit.plugin(paginateRest);
 
@@ -11,16 +14,13 @@ export default ({}: LoadOptions) => {
     const octokit = new OctokitWithPagination();
     return () => ({
         async getRepos(username: string){
-            const repos = [];
-            for await(const resp of octokit.paginate.iterator('GET /users/{username}/repos', { username })){
-                repos.push(...resp.data.map(({ id, name, html_url, language }) => ({
-                    id,
-                    name,
-                    url: html_url,
-                    language,
-                })));
-            }
-            return repos;
+            const res = await fromAsync(octokit.paginate.iterator('GET /users/{username}/repos', { username }));
+            return res.map(v => v.data).flat().map(({ id, name, html_url, language }) => ({
+                id,
+                name,
+                url: html_url,
+                language,
+            }));
         },
         async getUserData(username: string){
             for await(const resp of octokit.paginate.iterator('GET /users/{username}', { username })){
